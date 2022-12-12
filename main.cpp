4af87f5412e66bc0fd0883878d27b32f7235c76f
andreavallebona <115356293+andreavallebona@users.noreply.github.com>
2022-12-12 15:56:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "primaLibreria.h"
+using namespace std;
 
-int main(){
-  return 0;
+float x=0;
+
+int main() {
+    int d=0;
+    cin >> d;
+
+    float a[d];
+    for(int i=0; i<d; i++){
+        cin >> a[i];
+    }
+    for(int i=1; i<d; i++){
+        float n=max(a[i],a[i-1]);
+        if(n>x){
+            x=n;
+        }
+    }
+    cout << x;
+    return 0;
 }
